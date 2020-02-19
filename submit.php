96d0065b6b1d04f9e043ee684e2b726b0314bbc0
foxelas <foxeln@outlook.com>
2020-02-20 01:06:22 +0900

    add contact form

---
+++ submit.php
@@ -0,0 +1,61 @@
+<?php 
+$errors = '';
+$myemail = 'work.jpn@outlook.com';//<-----Put Your email address here.
+if(empty($_POST['name'])  || 
+   empty($_POST['email']) || 
+   empty($_POST['message']))
+{
+    $errors .= "\n Error: all fields are required";
+}
+
+if(!isset($_POST['submit']))
+{
+	$errors .= "\n Error: you need to submit the form";
+}
+		
+$name = $_POST['name']; 
+$email_address = $_POST['email']; 
+$message = $_POST['message']; 
+
+if (!preg_match(
+"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
+$email_address))
+{
+    $errors .= "\n Error: Invalid email address";
+}
+
+if( empty($errors))
+{
+	$to = $myemail; 
+	$email_subject = "foxelas.github - Contact form submission: $name";
+	$email_body = "You have received a new message. ".
+	" Here are the details:\n Name: $name \n Email: $email_address \n Message \n $message"; 
+	
+	$headers = "From: $myemail\n"; 
+	$headers .= "Reply-To: $email_address";
+	
+	if ($_POST['submit']) {	
+	   if (mail($to,$email_subject,$email_body,$headers)) {
+		echo '<p>Message Sent Successfully!</p>';
+		header('Location: index.html');
+	   } else {
+		echo '<p>Ah! Try again, please?</p>';
+	   }
+	}
+} 
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
+<html>
+<head>
+	<title>Contact form handler</title>
+</head>
+
+<body>
+<!-- This page is displayed only if there is some error -->
+<?php
+echo nl2br($errors);
+?>
+
+
+</body>
+</html>
