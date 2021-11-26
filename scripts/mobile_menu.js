c2784d03045f32a0f1648b7dc87058f286633109
foxelas <32893423+foxelas@users.noreply.github.com>
2021-11-26 12:48:14 +0900

    Update change cv structure

--- scripts/mobile_menu.js
+++ scripts/mobile_menu.js
@@ -5,3 +5,13 @@ function openNav() {
 function closeNav() {
   document.getElementById("mobileNav").style.width = "0%";
 }
+
+$(".certItem div.certContents").hide();
+
+$(".certItem button").click(function (e) {
+  e.preventDefault();
+  var $this = $(this).parent().find("div.certContents");
+  $(".certItem div.certContents").not($this).hide();
+
+  $this.toggle();
+});
